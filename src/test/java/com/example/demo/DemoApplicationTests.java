6760d296e4ca4a977e7960475a73d8960225e847
RodrigoHernandez26 <rodrigohsouza26@gmail.com>
2024-05-07 15:40:28 -0300

    add unit test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,23 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	private DemoController demoController;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testHelloWorld() {
+		String result = demoController.hello();
+		assert(result.equals("Hello, World!"));
 	}
 
 }
